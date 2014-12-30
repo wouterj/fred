cd9b5076d5998c4618b4f09588a787fb14d9c622
WouterJ <waldio.webdesign@gmail.com>
2014-12-30 15:02:25 +0100

    Finished Phpspec extension

--- ext/PhpSpec.php
+++ ext/PhpSpec.php
@@ -2,6 +2,8 @@
 
 namespace WouterJ\Fred\Extension;
 
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\ProcessBuilder;
 use WouterJ\Fred\File;
 use WouterJ\Fred\Iterator\MapIterator;
 use WouterJ\Fred\Step;
@@ -13,8 +15,20 @@ class PhpSpec implements Step
 {
     public function constructIterator(\Traversable $traversable)
     {
-        return new MapIterator($traversable, function (File $file) {
-            $file->setContent('passed');
+        $phpExecutable = new PhpExecutableFinder();
+        $phpExecutable = $phpExecutable->find();
+
+        return new MapIterator($traversable, function (File $file) use ($phpExecutable) {
+            $processBuilder = new ProcessBuilder();
+            $process = $processBuilder//->add($phpExecutable)
+                ->add(getcwd().'/vendor/bin/phpspec')
+                ->add('run')
+                ->add($file->info()->getRealPath())
+                ->getProcess();
+
+            $process->mustRun();
+
+            $file->setContent($process->getOutput());
 
             return $file;
         });
