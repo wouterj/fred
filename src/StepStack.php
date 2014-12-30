d6afcb02a8a893287608776c5590a6b26deaabef
WouterJ <waldio.webdesign@gmail.com>
2014-12-30 14:21:29 +0100

    First version of destination handling

--- src/StepStack.php
+++ src/StepStack.php
@@ -24,11 +24,21 @@ class StepStack
 
     public function dest($glob)
     {
-        var_dump('Iterator chain ended with dest to '.$glob);
+        if (STDOUT === $glob) {
+            foreach ($this->files as $file) {
+                fwrite(STDOUT, $file->content());
+            }
+        } elseif (is_dir($glob)) {
+            /** @var File $file */
+            foreach ($this->files as $file) {
+                file_put_contents($glob.DIRECTORY_SEPARATOR.$file->info()->getFilename(), $file->content());
+            }
+        } elseif (1 === count($this->files)) {
+            $file = current(iterator_to_array($this->files));
 
-        foreach ($this->files as $file) {
-            echo PHP_EOL, $file->info()->getRelativePathname();
-            echo ': ', $file->content();
+            file_put_contents(getcwd().DIRECTORY_SEPARATOR.$glob, $file->content());
+        } else {
+            throw new \InvalidArgumentException(sprintf('Unable to handle destination "%s".', $glob));
         }
     }
 }
